15ada7385217c0643c2f2a6ae36d72e09dc19ce1
Urs Grupp <urs.grupp@gmail.com>
2020-04-01 16:10:35 +0200

    feat: add styled-components
    refactor: use jsx extension

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,11 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import styled from "styled-components"
+
+const StyledLink = styled(Link)`
+  color: indianred;
+`
 
 const IndexPage = () => (
   <Layout>
@@ -14,7 +19,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <StyledLink to="/page-2/">Go to page 2</StyledLink>
   </Layout>
 )
 
